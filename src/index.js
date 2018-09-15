cb6516a0e96d35bd38774892bfae64e1d7b44256
Владислав Царапкин <scratchor@mail.ru>
2018-09-16 00:53:49 +0300

    task 2 ready

--- src/index.js
+++ src/index.js
@@ -4,4 +4,32 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+let triangles = 0;
+let z;
+let num = 0;
+let arr=[];
+function countTriangles(preferences, num) {
+  for (num; num < preferences.length; num++) {
+    let x = preferences[num];
+    let y = preferences[x - 1]
+    if(y && y !== x) {
+      z = preferences[y - 1];
+      } else {
+        continue;
+      }; 
+    if(z === (num + 1) && !arr.some(isEqual)) {
+      triangles++;
+      arr.splice(arr.length, 0, x, y, z);
+      continue;
+    } else {
+      continue;     
+    };
+  };
+  function isEqual(item) {
+     return item === z;
+  };
+  };
+  countTriangles(preferences, num);
+  return triangles;
 };
+
